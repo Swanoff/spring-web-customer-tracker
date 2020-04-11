eae68dcca8a9c629521f88ad15376579b6880443
Surya <suryathegreat.07@gmail.com>
2020-04-11 19:09:53 +0530

    Refactored by adding Service layer
    Added Service layer between Controller layer and DAO layer

---
+++ src/main/java/com/swanoff/springdemo/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.swanoff.springdemo.service;
+
+import java.util.List;
+
+import com.swanoff.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
