eab60cc5d29b6625f5d11d74f718fe0d3756d303
Surya <suryathegreat.07@gmail.com>
2020-04-16 13:22:24 +0530

    POST add customer

--- src/main/java/com/swanoff/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/swanoff/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +34,17 @@ public class CustomerRestController {
 		if (theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
+		return theCustomer;
+	}
+	
+	// add mapping for POST /customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		// also just-in-case if any id is passed in json ... set id to 0
+		// this is force a save of new item ... instead of update
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
 		return theCustomer;
 	}
 }
